4b3e5ee0b4a3e893fbb2e17f004dc2c10f400b4a
IAmASweet <117323062+IAmASweet@users.noreply.github.com>
2022-11-25 17:06:26 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const btnIncrement = document.querySelector(".btn-increment")
+const btnDecrement = document.querySelector(".btn-decrement")
+const clickNumber = document.querySelector(".number")
+let timesClicked = 0
+
+
+const increment = () => {
+  clickNumber.textContent = ++timesClicked
+}
+
+const decrement = () => {
+  if (timesClicked > 0) {
+    clickNumber.textContent = --timesClicked
+  }
+  
+}
+
+
+btnIncrement.addEventListener('click', increment)
+btnDecrement.addEventListener('click', decrement)
